0460fa884fb9635aad66ea25e013433e1f5dab1a
jcontour <contj186@newschool.edu>
2018-09-11 13:58:29 -0400

    adding physics lesson

---
+++ 3 Physics/Assets/physics.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class physics : MonoBehaviour {
+
+	public float upforce;
+
+	// Use this for initialization
+	void Start () {
+		upforce = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision other){
+//		Debug.Log (other.gameObject.tag);
+		if (other.gameObject.tag == "sphere") {
+			GetComponent<Renderer> ().material.color = Color.red;
+		}
+	}
+
+	void OnCollisionStay (Collision other){
+	}
+
+	void OnCollisionExit (Collision other){
+		if (other.gameObject.tag == "sphere") {
+			GetComponent<Renderer> ().material.color = Color.blue;
+		}
+	}
+
+	void OnMouseDown(){
+		GetComponent<Rigidbody> ().AddForce(Vector3.up * upforce);
+	}
+
+	void OnTriggerStay(Collider other){
+//		Debug.Log ("triggered");
+		GetComponent<Rigidbody> ().AddForce(Vector3.up * upforce);
+		GetComponent<Rigidbody> ().AddTorque (transform.right * 10);
+	}
+}
---
+++ 3 Physics/Assets/sphereGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereGame : MonoBehaviour {
+
+	public float moveforce;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+//		moveforce = 100f;
+		rb = GetComponent <Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	void FixedUpdate(){
+		if (Input.GetKeyDown(KeyCode.W)) {
+			rb.AddForce(Vector3.up * moveforce*2, ForceMode.Impulse);
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			rb.AddForce(Vector3.back * moveforce, ForceMode.Impulse);
+		}
+
+		if (Input.GetKeyDown(KeyCode.D)) {
+			rb.AddForce(Vector3.forward * moveforce, ForceMode.Impulse);
+		}
+	}
+
+}
