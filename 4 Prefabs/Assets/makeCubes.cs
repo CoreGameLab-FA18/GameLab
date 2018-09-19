d0da6a5e4886bf14b6c6aa0ac86ae118664b2504
jcontour <contj186@newschool.edu>
2018-09-19 17:16:00 -0400

    prefabs

---
+++ 4 Prefabs/Assets/makeCubes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeCubes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for (int y = 0; y < 5; y++) 
+		{
+			for (int x = 0; x < 5; x++) 
+			{
+				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				cube.AddComponent<Rigidbody>();
+				cube.transform.position = new Vector3(x, y, -5);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 4 Prefabs/Assets/makePrefabs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makePrefabs : MonoBehaviour {
+
+	public Transform brick;
+	// Use this for initialization
+	void Start () {
+		for (int y = 0; y < 5; y++)
+		{
+			for (int x = 0; x < 5; x++) 
+			{
+				Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
