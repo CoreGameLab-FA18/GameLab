78acc2d1e8303a77393f3c22328978c0bcf4287b
jcontour <contj186@newschool.edu>
2018-10-02 18:35:22 -0400

    ui lesson

---
+++ 7 UI/Assets/cubeAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cubeAction : MonoBehaviour {
+
+	public Slider slidey;
+	public Text winText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		// change UI
+		slidey.value = slidey.value + 1;
+
+		//change wintext
+		if (slidey.value <= 1) {
+			winText.text = "";
+		} else if (slidey.value == 5) {
+			winText.text = "You win!";
+		}
+			
+		Destroy (this.gameObject);
+	}
+
+
+
+}
---
+++ 7 UI/Assets/timerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerScript : MonoBehaviour {
+
+	float timeTracker;
+	Slider s;
+	public Text wintext;
+
+	// Use this for initialization
+	void Start () {
+
+		s = GetComponent<Slider> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeTracker = Time.time;
+		s.value = timeTracker;
+
+		if (s.value >= 10) {
+			wintext.text = "You LOSE";
+		}
+
+	}
+}
