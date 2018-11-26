825637b0a854fa64cfdd5756179cada7023ea08e
jcontour <contj186@newschool.edu>
2018-11-26 16:59:59 -0500

    patrol and chase

---
+++ 14 Patrolling/Assets/patrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol : MonoBehaviour {
+
+	public float speed;
+	public Transform[] moveSpots;
+	private int randomSpot;
+
+	private float waitTime;
+	public float startWaitTime;
+
+	public GameObject player;
+	public float distCheck;
+
+	// Use this for initialization
+	void Start () {
+		waitTime = startWaitTime;
+		randomSpot = Random.Range (0, moveSpots.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Vector3.Distance (transform.position, player.transform.position) < distCheck) {
+		
+			transform.position = Vector3.MoveTowards (transform.position, player.transform.position, speed * Time.deltaTime);
+		
+		} else {
+
+			transform.position = Vector3.MoveTowards (transform.position, moveSpots [randomSpot].position, speed * Time.deltaTime);
+
+			if (Vector3.Distance (transform.position, moveSpots [randomSpot].position) < 0.2f) {
+				if (waitTime <= 0) {
+					randomSpot = Random.Range (0, moveSpots.Length);
+					waitTime = startWaitTime;
+				} else {
+					waitTime -= Time.deltaTime;
+				}
+			}
+		}
+
+	}
+}
---
+++ 14 Patrolling/Assets/playerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+
+	int speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float vert = Input.GetAxis ("Vertical")*speed;
+		float horz = Input.GetAxis ("Horizontal")*speed;
+
+		vert *= Time.deltaTime;
+		horz *= Time.deltaTime;
+
+		transform.Translate(horz, vert, 0);
+		
+	}
+}
