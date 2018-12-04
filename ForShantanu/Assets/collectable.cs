becd059881cf25a469989ac1f0e23cdbe09000f0
jcontour <contj186@newschool.edu>
2018-12-04 16:48:11 -0500

    for shantanu

---
+++ ForShantanu/Assets/collectable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectable : MonoBehaviour {
+
+	public GameObject wall;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = wall.GetComponent<Animator> ();
+	}
+		
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("triggered");
+		anim.SetTrigger ("moveWall");
+		Destroy (this.gameObject);
+	}
+}
